6d470aed61e84d00e52256862abc709c4ee3c01e
angeleskinikola <46307266+angeleskinikola@users.noreply.github.com>
2020-09-17 19:01:20 +0200

    Add files via upload

---
+++ WordCounter/src/main/java/Application.java
@@ -0,0 +1,37 @@
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class Application {
+
+    public static void main(String[] args) throws IOException {
+        printMap(wordCounter(new File("file.txt")));
+    }
+
+    public static HashMap<String, Integer> wordCounter(File file) throws IOException {
+        String text = FileUtils.readFileToString(file, "UTF-8");
+        text = text.toLowerCase();
+        String clearText = text.replaceAll("[^a-zA-Z ]", "");
+        String[] words = clearText.split(" ");
+
+        HashMap<String, Integer> wordCountMap = new HashMap<String, Integer>();
+        for (String word : words) {
+            if (wordCountMap.containsKey(word)) {
+                wordCountMap.put(word, wordCountMap.get(word) + 1);
+            } else {
+                wordCountMap.put(word, 1);
+            }
+        }
+
+        return wordCountMap;
+    }
+
+    public static void printMap(HashMap<String, Integer> map) {
+        System.out.println("word: count");
+        for (String word : map.keySet()) {
+            System.out.println(word + ": " + map.get(word));
+        }
+    }
+}
