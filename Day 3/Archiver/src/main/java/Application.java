de01d40e176f8b9df1b5b92617a62a642319fa22
angeleskinikola <46307266+angeleskinikola@users.noreply.github.com>
2020-09-22 12:23:07 +0200

    Update Application.java

--- Day 3/Archiver/src/main/java/Application.java
+++ Day 3/Archiver/src/main/java/Application.java
@@ -5,24 +5,24 @@ import java.util.Scanner;
 public class Application {
 
     public static void main(String[] args) throws IOException {
-        ZipArchiver zipArchiver = new ZipArchiver();
-        SevenZipArchiver sevenZipArchiver = new SevenZipArchiver();
         File directory = new File("files");
-        File zipArchiveFile = new File("archive.zip");
-        File severZipArchiveFile = new File("archive.7z");
 
         System.out.println("Type zip to be created zip archives, or type 7z to be created 7z archives.");
         String answer = new Scanner(System.in).nextLine();
 
         if (answer.equalsIgnoreCase("zip")) {
-            zipArchiver.archiver(directory, zipArchiveFile);
+            ZipArchiver zipArchiver = new ZipArchiver();
+            File zipArchiveFile = new File("archive.zip");
+            zipArchiver.archive(directory, zipArchiveFile);
             System.out.println("zip file successfully created!");
         } else if (answer.equalsIgnoreCase("7z")) {
-            sevenZipArchiver.archiver(directory, severZipArchiveFile);
+            SevenZipArchiver sevenZipArchiver = new SevenZipArchiver();
+            File severZipArchiveFile = new File("archive.7z");
+            sevenZipArchiver.archive(directory, severZipArchiveFile);
             System.out.println("7-zip file successfully created!");
+
         } else {
             System.out.println("You should type zip or 7z!");
         }
-
     }
 }
