88c82d630a45d90bb528b91068f707dc2b537221
angeleskinikola <46307266+angeleskinikola@users.noreply.github.com>
2020-09-26 17:28:08 +0200

    Add files via upload

--- Day 6/WordSorterWithLambdaAndFilterWithTests/src/test/java/ApplicationTest.java
+++ Day 6/WordSorterWithLambdaAndFilterWithTests/src/test/java/ApplicationTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.LinkedHashSet;
@@ -6,33 +7,36 @@ import java.util.Set;
 
 class ApplicationTest {
 
-    @Test
-    void sortAndFilterWords_ascSortingType() {
-        Set<String> words = new LinkedHashSet<>();
+    private Set<String> words;
+
+    @BeforeEach
+    void setUp() {
+        words = new LinkedHashSet<>();
         words.add("abc");
         words.add("c");
+        words.add("abcd");
         words.add("bc");
+    }
 
-        Set<String> result = Application.sortAndFilterWords(words, "asc", 1, 3);
+    @Test
+    void sortAndFilterWords_ascSortingType() {
+        Set<String> result = Application.sortAndFilterWords(words, "asc", 1, 4);
 
         Set<String> expectedWordsOrder = new LinkedHashSet<>();
         expectedWordsOrder.add("c");
         expectedWordsOrder.add("bc");
         expectedWordsOrder.add("abc");
+        expectedWordsOrder.add("abcd");
 
         Assertions.assertIterableEquals(expectedWordsOrder, result);
     }
 
     @Test
     void sortAndFilterWords_descSortingType() {
-        Set<String> words = new LinkedHashSet<>();
-        words.add("abc");
-        words.add("c");
-        words.add("bc");
-
-        Set<String> result = Application.sortAndFilterWords(words, "desc", 1, 3);
+        Set<String> result = Application.sortAndFilterWords(words, "desc", 1, 4);
 
         Set<String> expectedWordsOrder = new LinkedHashSet<>();
+        expectedWordsOrder.add("abcd");
         expectedWordsOrder.add("abc");
         expectedWordsOrder.add("bc");
         expectedWordsOrder.add("c");
@@ -42,23 +46,12 @@ class ApplicationTest {
 
     @Test
     void sortAndFilterWords_invalidSortingType() {
-        Set<String> words = new LinkedHashSet<>();
-        words.add("abc");
-        words.add("c");
-        words.add("bc");
-
         Assertions.assertThrows(IllegalArgumentException.class, () -> Application
-                .sortAndFilterWords(words, "up", 1, 3), "You should type asc or desc!");
+                .sortAndFilterWords(words, "up", 1, 4), "You should type asc or desc!");
     }
 
     @Test
     void sortAndFilterWords_notRestrictedWordLength() {
-        Set<String> words = new LinkedHashSet<>();
-        words.add("abc");
-        words.add("c");
-        words.add("bc");
-        words.add("abcd");
-
         Set<String> result = Application.sortAndFilterWords(words, "asc", 0, 0);
 
         Set<String> expectedWordsOrder = new LinkedHashSet<>();
@@ -72,12 +65,6 @@ class ApplicationTest {
 
     @Test
     void sortAndFilterWords_restrictedMaxWordLength() {
-        Set<String> words = new LinkedHashSet<>();
-        words.add("abc");
-        words.add("c");
-        words.add("bc");
-        words.add("abcd");
-
         Set<String> result = Application.sortAndFilterWords(words, "asc", 0, 3);
 
         Set<String> expectedWordsOrder = new LinkedHashSet<>();
@@ -90,12 +77,6 @@ class ApplicationTest {
 
     @Test
     void sortAndFilterWords_restrictedMinWordLength() {
-        Set<String> words = new LinkedHashSet<>();
-        words.add("abc");
-        words.add("c");
-        words.add("bc");
-        words.add("abcd");
-
         Set<String> result = Application.sortAndFilterWords(words, "asc", 2, 0);
 
         Set<String> expectedWordsOrder = new LinkedHashSet<>();
@@ -108,12 +89,6 @@ class ApplicationTest {
 
     @Test
     void sortAndFilterWords_restrictedMinMaxWordLength() {
-        Set<String> words = new LinkedHashSet<>();
-        words.add("abc");
-        words.add("c");
-        words.add("bc");
-        words.add("abcd");
-
         Set<String> result = Application.sortAndFilterWords(words, "asc", 2, 3);
 
         Set<String> expectedWordsOrder = new LinkedHashSet<>();
